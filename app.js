ddb1b6aeae4bcecf50e4bc67620c0e4fecf0dac4
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-03-31 21:27:45 +0200

    added couple style randome stuff on top

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express")
-const PORT = 9001
+const PORT = 9002
 
 const app = express()
 
-// app.use((req, _, next) => {
-//     console.log("neue request", req.method, req.url);
-//     next()
-// })
+app.use((req, _, next) => {
+    console.log("neue request", req.method, req.url);
+    next()
+})
 
+app.use(express.static("views"))
 app.use(express.static("public"))
 
 app.use((_, res) => {
     res.status(404)
-    res.sendFile(__dirname + "/public/404.html")
+    res.sendFile(__dirname + "/views/404.html")
 })
 
 app.listen(PORT, console.log("Server is listing to Port ", PORT))
